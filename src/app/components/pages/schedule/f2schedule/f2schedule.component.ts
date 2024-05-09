94c46f8483c7e1dc78eb3f8f21db7e4e4af75203
Pablo Pitarch <ppitarch01@gmail.com>
2024-05-09 23:55:20 +0200

    add f2 schedule

--- src/app/components/pages/schedule/f2schedule/f2schedule.component.ts
+++ src/app/components/pages/schedule/f2schedule/f2schedule.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '../../../../pipes/datepipe.pipe';
+import { CountryCode } from '../../../../enum/flags';
+import { Schedule } from '../../../../model/schedule';
+import { f2schedule } from './f2schedule';
 
 @Component({
   selector: 'app-f2schedule',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './f2schedule.component.html',
   styleUrl: './f2schedule.component.css'
 })
 export class F2scheduleComponent {
 
+  countries = CountryCode;
+  f2Schedule = f2schedule as any;
+
+  ngOnInit() {
+    this.f2Schedule.forEach((race: Schedule) => {
+      let isoTime = new Date(race.date).toISOString().split('T')[0] + "T" + race.time; // Add current date to the time string
+      let date = new Date(isoTime);
+      let offset = date.getTimezoneOffset();
+      date.setHours(date.getHours() - offset / 60);
+      if (date < new Date()) {
+        race.past = true;
+      }
+      race.time = date.toISOString().split('T')[1].slice(0, 5);
+    });
+  }
+  getCode(country: string) {
+    country = country.replace(" ", "");
+    return "fi-" + this.countries[country as keyof typeof CountryCode];
+  }
 }
---
+++ src/app/components/pages/schedule/f2schedule/f2schedule.ts
@@ -0,0 +1,269 @@
+export const f2schedule = [
+    {
+        "season": "2024",
+        "round": "1",
+        "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Bahrain_Grand_Prix",
+        "raceName": "Bahrain Grand Prix",
+        "Circuit": {
+            "circuitId": "bahrain",
+            "url": "http:\/\/en.wikipedia.org\/wiki\/Bahrain_International_Circuit",
+            "circuitName": "Bahrain International Circuit",
+            "Location": {
+                "lat": "26.0325",
+                "long": "50.5106",
+                "locality": "Sakhir",
+                "country": "Bahrain"
+            }
+        },
+        "date": "2024-03-02",
+        "time": "10:30:00Z",
+    },
+    {
+
+        "season": "2024",
+        "round": "2",
+        "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Saudi_Arabian_Grand_Prix",
+        "raceName": "Saudi Arabian Grand Prix",
+        "Circuit": {
+            "circuitId": "jeddah",
+            "url": "http:\/\/en.wikipedia.org\/wiki\/Jeddah_Street_Circuit",
+            "circuitName": "Jeddah Corniche Circuit",
+            "Location": {
+                "lat": "21.6319",
+                "long": "39.1044",
+                "locality": "Jeddah",
+                "country": "Saudi Arabia"
+            }
+        },
+        "date": "2024-03-09",
+        "time": "13:30:00Z"
+    },
+    {
+        "season": "2024",
+                    "round": "3",
+                    "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Australian_Grand_Prix",
+                    "raceName": "Australian Grand Prix",
+                    "Circuit": {
+                        "circuitId": "albert_park",
+                        "url": "http:\/\/en.wikipedia.org\/wiki\/Melbourne_Grand_Prix_Circuit",
+                        "circuitName": "Albert Park Grand Prix Circuit",
+                        "Location": {
+                            "lat": "-37.8497",
+                            "long": "144.968",
+                            "locality": "Melbourne",
+                            "country": "Australia"
+                        }
+                    },
+                    "date": "2024-03-24",
+                    "time": "00:35:00Z",
+    },
+    {
+        "season": "2024",
+        "round": "4",
+        "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Emilia_Romagna_Grand_Prix",
+        "raceName": "Emilia Romagna Grand Prix",
+        "Circuit": {
+            "circuitId": "imola",
+            "url": "http:\/\/en.wikipedia.org\/wiki\/Autodromo_Enzo_e_Dino_Ferrari",
+            "circuitName": "Autodromo Enzo e Dino Ferrari",
+            "Location": {
+                "lat": "44.3439",
+                "long": "11.7167",
+                "locality": "Imola",
+                "country": "Italy"
+            }
+        },
+        "date": "2024-05-19",
+        "time": "08:00:00Z",
+    },
+    {
+        "season": "2024",
+                    "round": "5",
+                    "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Monaco_Grand_Prix",
+                    "raceName": "Monaco Grand Prix",
+                    "Circuit": {
+                        "circuitId": "monaco",
+                        "url": "http:\/\/en.wikipedia.org\/wiki\/Circuit_de_Monaco",
+                        "circuitName": "Circuit de Monaco",
+                        "Location": {
+                            "lat": "43.7347",
+                            "long": "7.42056",
+                            "locality": "Monte-Carlo",
+                            "country": "Monaco"
+                        }
+                    },
+                    "date": "2024-05-26",
+                    "time": "07:40:00Z"
+    },
+    {
+        "season": "2024",
+        "round": "6",
+        "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Spanish_Grand_Prix",
+        "raceName": "Spanish Grand Prix",
+        "Circuit": {
+            "circuitId": "catalunya",
+            "url": "http:\/\/en.wikipedia.org\/wiki\/Circuit_de_Barcelona-Catalunya",
+            "circuitName": "Circuit de Barcelona-Catalunya",
+            "Location": {
+                "lat": "41.57",
+                "long": "2.26111",
+                "locality": "Montmeló",
+                "country": "Spain"
+            }
+        },
+        "date": "2024-06-23",
+        "time": "08:40:00Z"
+    },
+    {
+        "season": "2024",
+                    "round": "7",
+                    "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Austrian_Grand_Prix",
+                    "raceName": "Austrian Grand Prix",
+                    "Circuit": {
+                        "circuitId": "red_bull_ring",
+                        "url": "http:\/\/en.wikipedia.org\/wiki\/Red_Bull_Ring",
+                        "circuitName": "Red Bull Ring",
+                        "Location": {
+                            "lat": "47.2197",
+                            "long": "14.7647",
+                            "locality": "Spielberg",
+                            "country": "Austria"
+                        }
+                    },
+                    "date": "2024-06-30",
+                    "time": "09:40:00Z"
+    },
+    {
+        "season": "2024",
+        "round": "8",
+        "url": "https:\/\/en.wikipedia.org\/wiki\/2024_British_Grand_Prix",
+        "raceName": "British Grand Prix",
+        "Circuit": {
+            "circuitId": "silverstone",
+            "url": "http:\/\/en.wikipedia.org\/wiki\/Silverstone_Circuit",
+            "circuitName": "Silverstone Circuit",
+            "Location": {
+                "lat": "52.0786",
+                "long": "-1.01694",
+                "locality": "Silverstone",
+                "country": "UK"
+            }
+        },
+        "date": "2024-07-07",
+        "time": "09:40:00Z"
+    },
+    {
+        "season": "2024",
+                    "round": "9",
+                    "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Hungarian_Grand_Prix",
+                    "raceName": "Hungarian Grand Prix",
+                    "Circuit": {
+                        "circuitId": "hungaroring",
+                        "url": "http:\/\/en.wikipedia.org\/wiki\/Hungaroring",
+                        "circuitName": "Hungaroring",
+                        "Location": {
+                            "lat": "47.5789",
+                            "long": "19.2486",
+                            "locality": "Budapest",
+                            "country": "Hungary"
+                        }
+                    },
+                    "date": "2024-07-21",
+                    "time": "08:40:00Z"
+    },
+    {
+        "season": "2024",
+                    "round": "10",
+                    "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Belgian_Grand_Prix",
+                    "raceName": "Belgian Grand Prix",
+                    "Circuit": {
+                        "circuitId": "spa",
+                        "url": "http:\/\/en.wikipedia.org\/wiki\/Circuit_de_Spa-Francorchamps",
+                        "circuitName": "Circuit de Spa-Francorchamps",
+                        "Location": {
+                            "lat": "50.4372",
+                            "long": "5.97139",
+                            "locality": "Spa",
+                            "country": "Belgium"
+                        }
+                    },
+                    "date": "2024-07-28",
+                    "time": "08:40:00Z"
+    },
+    {
+        "season": "2024",
+                    "round": "11",
+                    "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Italian_Grand_Prix",
+                    "raceName": "Italian Grand Prix",
+                    "Circuit": {
+                        "circuitId": "monza",
+                        "url": "http:\/\/en.wikipedia.org\/wiki\/Autodromo_Nazionale_Monza",
+                        "circuitName": "Autodromo Nazionale di Monza",
+                        "Location": {
+                            "lat": "45.6156",
+                            "long": "9.28111",
+                            "locality": "Monza",
+                            "country": "Italy"
+                        }
+                    },
+                    "date": "2024-09-01",
+                    "time": "08:40:00Z"
+    },
+    {
+        "season": "2024",
+                    "round": "12",
+                    "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Azerbaijan_Grand_Prix",
+                    "raceName": "Azerbaijan Grand Prix",
+                    "Circuit": {
+                        "circuitId": "baku",
+                        "url": "http:\/\/en.wikipedia.org\/wiki\/Baku_City_Circuit",
+                        "circuitName": "Baku City Circuit",
+                        "Location": {
+                            "lat": "40.3725",
+                            "long": "49.8533",
+                            "locality": "Baku",
+                            "country": "Azerbaijan"
+                        }
+                    },
+                    "date": "2024-09-15",
+                    "time": "06:40:00Z"
+    },
+    {
+        "season": "2024",
+                    "round": "13",
+                    "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Qatar_Grand_Prix",
+                    "raceName": "Qatar Grand Prix",
+                    "Circuit": {
+                        "circuitId": "losail",
+                        "url": "http:\/\/en.wikipedia.org\/wiki\/Losail_International_Circuit",
+                        "circuitName": "Losail International Circuit",
+                        "Location": {
+                            "lat": "25.49",
+                            "long": "51.4542",
+                            "locality": "Al Daayen",
+                            "country": "Qatar"
+                        }
+                    },
+                    "date": "2024-12-01",
+                    "time": "07:40:00Z"
+    },
+    {
+        "season": "2024",
+                    "round": "14",
+                    "url": "https:\/\/en.wikipedia.org\/wiki\/2024_Abu_Dhabi_Grand_Prix",
+                    "raceName": "Abu Dhabi Grand Prix",
+                    "Circuit": {
+                        "circuitId": "yas_marina",
+                        "url": "http:\/\/en.wikipedia.org\/wiki\/Yas_Marina_Circuit",
+                        "circuitName": "Yas Marina Circuit",
+                        "Location": {
+                            "lat": "24.4672",
+                            "long": "54.6031",
+                            "locality": "Abu Dhabi",
+                            "country": "UAE"
+                        }
+                    },
+                    "date": "2024-12-08",
+                    "time": "06:40:00Z"
+    }
+]
